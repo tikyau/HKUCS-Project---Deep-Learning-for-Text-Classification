e5d709ec8a7d8bb25df1fc114b99cb5e2da3d36a
Zhihan CHEN <CHEN.Zhihan@outlook.com>
2018-04-08 13:24:56 +0000

    change search cnn

--- search.py
+++ search.py
@@ -9,20 +9,14 @@ from models import LSTMClassificationWrapper, LSTMRegressionWrapper,\
 
 
 def search_lstm(data_manager, log_path, args):
-    try_embedding = 10
-    try_lstm = 5
     best_embedding = 0
     best_lstm = 0
     best_result = 0
     best_reducer = None
     all_result = []
-    reducers = {"reduce_max": C.sequence.reduce_max,
-                "reduce_sum": C.sequence.reduce_sum,
-                "last": C.sequence.last}
-    for i in range(try_embedding):
-        embedding = (i + 1) * 100
-        for l in range(try_lstm):
-            lstm = (l + 1) * 200
+    reducers = {"reduce_sum": C.sequence.reduce_sum}
+    for embedding in range(1000, 1300, 100):
+        for lstm in range(1000, 1300, 100):
             for reducer in reducers:
                 j = {"embedding": embedding, "lstm": lstm, "reducer": reducer}
                 new_log_path = os.path.join(
@@ -53,26 +47,19 @@ def search_lstm(data_manager, log_path, args):
 
 
 def search_cnn(data_manager, log_path, args):
-    try_embedding = 10
-    try_conv_layers = 10
+    try_conv_layers = 5
     try_conv_size_range = (2, 5)
-    best_embedding = 0
-    best_layer = 0
-    best_resut = 0
-    best_size = 0
-    best_reducer = None
     all_result = []
     reducers = {"reduce_max": C.sequence.reduce_max,
                 "reduce_sum": C.sequence.reduce_sum}
-    for _ in range(try_embedding):
-        embedding = random.randrange(200, 1000, 20)
-        for conv_layer in range(1, try_conv_layers):
-            for conv_size in range(try_conv_size_range[0], try_conv_size_range[1]):
-                for reducer in reducers:
-                    j = {"embedding": embedding,
-                         "conv_layer": conv_layer,
-                         "conv_size": conv_size,
-                         "reducer": reducer}
+    embedding = 1000
+    for conv_layer in range(1, try_conv_layers):
+        for conv_size in range(try_conv_size_range[0], try_conv_size_range[1]):
+            for reducer in reducers:
+                j = {"embedding": embedding,
+                     "conv_layer": conv_layer,
+                     "conv_size": conv_size,
+                     "reducer": reducer}
                 new_log_path = os.path.join(
                     log_path, "{}_{}_{}_{}".format(embedding, conv_layer,
                                                    conv_size, reducer))
@@ -87,15 +74,9 @@ def search_cnn(data_manager, log_path, args):
                 print("{}\taccuracy: {:2f}".format(j, result))
                 j["accuracy"] = result
                 all_result.append(j)
-                if result > best_result:
-                    best_result = result
-                    best_embedding = embedding
-                    best_layer = conv_layer
-                    best_size = conv_size
-                    best_reducer = reducer
-                with open(os.path.join(log_path, "result.json"), "w") as f:
+                with open(os.path.join(log_path, "cnn.json"), "w") as f:
                     json.dump(all_result, f)
 
 
 def search_model(data_manager, log_path, args):
-    search_lstm(data_manager, log_path, args)
+    search_cnn(data_manager, log_path, args)
